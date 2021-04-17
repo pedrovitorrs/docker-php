ace99e9d2c5a9f8d3b27655e53ee37d20006beb1
pedrovitorrs <pedro442.rodrigues@gmail.com>
2021-04-17 15:25:03 -0300

    feat: add a config container mysql and adminer

--- html/index.php
+++ html/index.php
@@ -1,6 +1,15 @@
 <?php
-
-# Teste
-echo "oi"
-
+    $banco = "pagplay";
+    $usuario = "root";
+    $senha = "MySql2019!";
+    $hostname = "mysqlsrv";
+    $conn = mysqli_connect($hostname,$usuario,$senha); 
+    mysqli_select_db($conn,$banco) or die( "Não foi possível conectar ao banco MySQL");
+    if (!$conn) { 
+        echo "Não foi possível conectar ao banco MySQL.<br>"; 
+        exit;
+    }else {
+        echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.<br>";
+    }
+    mysqli_close($conn);
 ?>
